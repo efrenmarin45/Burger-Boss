43b3d27dd3657dde2d90af71254430bb2d1fe66c
Efren_Marin <efren45marin@gmail.com>
2020-03-01 20:21:02 -0800

    Added connection fileto hook up to MYSQL

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Setting up MySQL connection.
+const mysql = require("mysql");
+const connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Wander89!",
+    database: "burger_db"
+  });
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("Error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
