7f5b7c2e9b1fe3534040c3949c46389d13d1c87f
Efren_Marin <efren45marin@gmail.com>
2020-03-01 21:08:11 -0800

    Worked on models and controllers for project.

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+const orm = require("../config/orm");
+
+const burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+
+
+  module.exports = burger;
