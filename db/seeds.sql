f42f66c0df11a28bf9333c8512cc2cd544933f96
Efren_Marin <efren45marin@gmail.com>
2020-03-01 19:56:19 -0800

    Added initial seeds for testing

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double Double");
+INSERT INTO burgers (burger_name)
+VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name)
+VALUES ("Impossible Whopper");
+
